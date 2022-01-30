a59f30f7ec0c9600de43ec8a12c384fe3faa0229
Steve Wasiswa <stevewasiswa@gmail.com>
2022-01-30 09:03:39 -0600

    Modify urls to serve pages created with React

--- administrators/urls.py
+++ administrators/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from administrators import views
+from django.views.generic import TemplateView
 
 # Customization of django admin
 admin.site.site_header = "Team Cash's Parking"
@@ -8,6 +9,6 @@ admin.site.site_title = "Welcome to Team Cash's Dashboard"
 admin.site.index_title = "Welcome to the Portal"
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
+    path('', TemplateView.as_view(template_name='index.html')),
+    #path('home/', views.home, name='home'),
 ]
