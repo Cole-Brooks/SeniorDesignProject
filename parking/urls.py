2fc078f4117bf3be04daf7bf14d62f3233fdcb1b
Steve Wasiswa <stevewasiswa@gmail.com>
2022-02-08 13:04:09 -0600

    Add new urls

--- parking/urls.py
+++ parking/urls.py
@@ -17,13 +17,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('administrators.urls'))
+    path('', include('administrators.urls')),
+    path('customers/', include('customers.urls')),
+    path('users/', include('users.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
