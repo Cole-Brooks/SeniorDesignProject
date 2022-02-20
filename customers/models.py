64c7d2a876fcf38c681f266af4a78fd4529815b2
Steve Wasiswa <stevewasiswa@gmail.com>
2022-02-20 11:49:51 -0600

    Design models related to customers

--- customers/models.py
+++ customers/models.py
@@ -15,10 +15,10 @@ class Car(models.Model):
     """This is the Car user model"""
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    mark = models.CharField(blank=False, null=False, max_length=255)
+    make = models.CharField(blank=False, null=False, max_length=255)
     model = models.CharField(blank=False, null=False, max_length=255)
-    licence_plate_number = models.CharField(blank=False, null=False, max_length=255)
-    State = models.CharField(blank=False, null=False, max_length=255)
+    license_plate_number = models.CharField(blank=False, null=False, max_length=255)
+    state = models.CharField(blank=False, null=False, max_length=255)
 
     class Meta:
         verbose_name = 'Car'
