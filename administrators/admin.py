354dee0c05fc879fb48402d40232b50bf5726585
Steve Wasiswa <stevewasiswa@gmail.com>
2022-02-20 11:48:32 -0600

    Register models related to administrators

--- administrators/admin.py
+++ administrators/admin.py
@@ -6,3 +6,10 @@ from .models import ParkingLot
 @admin.register(ParkingLot)
 class ParkingAdmin(admin.ModelAdmin):
     list_display = ('id', 'administrator', 'parking_name', 'phone')
+
+    actions = None
+
+    def save_model(self, request, obj, form, change):
+        if not obj.administrator:
+            obj.administrator = request.user
+        obj.save()
