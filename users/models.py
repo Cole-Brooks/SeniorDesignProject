81eb888a25f7e2a45330cdf26cc8f5e67a800df9
Steve Wasiswa <stevewasiswa@gmail.com>
2022-02-20 11:50:23 -0600

    Add models related to users

--- users/models.py
+++ users/models.py
@@ -13,14 +13,14 @@ from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 class User(AbstractUser):
     """This is the administrator user model"""
-    #parking_administrator = models.BooleanField(default=False)
+    # parking_administrator = models.BooleanField(default=False)
     USER_TYPES = (
         [('Parking Administrator', 'Parking Administrator'),
-        ('Customer', 'Customer'),])
+         ('Customer', 'Customer'), ])
     Please_check_this_if_you_are_a_parking_administrator = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email',]
+    REQUIRED_FIELDS = ['email', ]
 
     class Meta:
         verbose_name = 'User'
@@ -30,4 +30,16 @@ class User(AbstractUser):
         return self.first_name + " " + self.last_name
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
 
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
+
+    @property
+    def get_avatar_url(self):
+        if self.photo and hasattr(self.photo, 'url'):
+            return self.photo.url
+        else:
+            return "https://res.cloudinary.com/dh13i9dce/image/upload/v1642216377/media/avatars/defaultprofile_vad1ub.png"
