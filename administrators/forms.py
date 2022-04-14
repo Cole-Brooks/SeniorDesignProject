3d2455fd4eae2cfa982e4b85639394e660b938d1
Steve Wasiswa <stevewasiswa@gmail.com>
2022-04-14 12:39:45 -0500

    Add new forms and refactor

--- administrators/forms.py
+++ administrators/forms.py
@@ -2,11 +2,12 @@ from django import forms
 from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
 from .models import ParkingLot
+from localflavor.us.forms import USZipCodeField
 
 
 class RegisterParkingForm(forms.ModelForm):
     """Login form to for adding a parking"""
-
+    zip_code = USZipCodeField()
     class Meta:
         model = ParkingLot
         fields = ('administrator', 'parking_name', 'street_address', 'city', 'state', 'zip_code', 'phone',
--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.conf import settings
 from django.utils.translation import gettext, gettext_lazy as _
 from django.db import models
-from users.models import User, Profile
+from users.models import User, Profile, Contact
 from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
@@ -90,4 +90,53 @@ class UserProfileForm(forms.ModelForm):
         fields = ('photo',)
 
 
+class ContactForm(forms.ModelForm):
+    """Form for the contact"""
 
+    name = forms.CharField(label="Name", max_length=250, required=True, widget=forms.TextInput(
+        attrs={'placeholder': 'Full name', 'class': 'form-control'}))
+    email = forms.EmailField(label="Email", max_length=250, required=True, widget=forms.TextInput(
+        attrs={'placeholder': 'Email', 'class': 'form-control'}))
+    phone = PhoneNumberField(label="Phone number", widget=PhoneNumberPrefixWidget(initial='US',
+                                                            attrs={'placeholder': 'Phone number', 'class': 'form'
+                                                                                                           '-control'}),
+                             required=False)
+
+    subject = forms.CharField(label="Subject", max_length=250, required=False, widget=forms.TextInput(
+        attrs={'placeholder': 'Subject', 'class': 'form-control'}))
+    message = forms.CharField(label="Message", max_length=2000, required=True, widget=forms.Textarea(
+        attrs={'placeholder': 'Your message', 'rows': 7, 'class': 'form-control'}))
+
+    def get_message(self):
+
+        clean_data = super().clean()
+
+        name = clean_data.get('name')
+        email = clean_data.get('email')
+        phone = clean_data.get('phone')
+        subject = clean_data.get('subject')
+        message_to_send = clean_data.get('message')
+
+        message = f'You got a new message:\n\n'
+        message += f'Sender Info:\n'
+        message += f'\n"Subject : {subject}"'
+        message += f'\n"Name : {name}"'
+        message += f'\n"Email : {email}"'
+        message += f'\n"Phone number : {phone}"\n\n'
+        message += f'\n{message_to_send}\n\n'
+
+        return message, subject
+
+    def send_email(self):
+
+        message, subject = self.get_message()
+
+        try:
+            send_mail(subject, message, from_email=str(settings.ADMIN_EMAIL),
+                      recipient_list=[str(settings.ADMIN_EMAIL)], fail_silently=False, )
+        except BadHeaderError:
+            return HttpResponse('Invalid header found')
+
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'phone', 'subject', 'message',)
